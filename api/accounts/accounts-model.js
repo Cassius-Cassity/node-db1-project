af7acd4076365abcdb0972cbd203c6ab5be936dc
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2022-03-21 20:34:33 -0600

    MVP done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,24 +1,28 @@
 const db = require("../../data/db-config");
 
 const getAll = () => {
-return db('accounts')
-
+  return db('accounts');
+  // select * from accounts
 }
 
 const getById = id => {
 return db('accounts').where("id", id).first()
+  // select * from accounts where id = 1
 }
 
-const create = account => {
-
+const create = async account => {
+const [id] = await db('accounts').insert(account)
+return getById(id)
 }
 
-const updateById = (id, account) => {
-
+const updateById = async (id, account) => {
+await db('accounts').where('id', id).update(account)
+return getById(id)
 }
 
 const deleteById = id => {
-
+  //delete from accounts where id = 1
+  return db('account').where('id', id).del()
 }
 
 module.exports = {
